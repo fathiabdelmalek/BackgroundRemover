4b1f177158e043365ff915306e1459c7507b77b4
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2024-08-22 23:24:40 +0100

    add gunicorn, improved UI, upload u2net.onnx to docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM python:3.9
+FROM python:3.9-slim
 COPY u2net.onnx /home/.u2net/u2net.onnx
-WORKDIR /app
-COPY . .
+WORKDIR /user/src/app
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-EXPOSE 5100
-CMD ["python", "app.py"]
+COPY . .
+EXPOSE 5000
+CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
--- app.py
+++ app.py
@@ -1,21 +1,25 @@
+import os
 from io import BytesIO
 
-from flask import Flask, render_template, request, send_file
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, send_file, Response
 from rembg import remove
 from PIL import Image
 
+
+load_dotenv()
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def upload_file() -> str | Response:
     if request.method == 'POST':
         if 'file' not in request.files:
-            return 'No file uploaded', 400
+            return Response('No file uploaded', 400)
         file = request.files['file']
         if file.filename == '':
-            return 'No file selected', 400
+            return Response('No file selected', 400)
         if file:
             input_image = Image.open(file.stream)
             output_image = remove(input_image, post_process_mask=True)
@@ -27,4 +31,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5100)
+    app.run(host='0.0.0.0', port=5000, debug=os.getenv("DEBUG"))
